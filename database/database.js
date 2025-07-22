c4b16005c9d9b9d92de95ecfb8aff7fd332090e2
Sol Requiao Falcao <solrequiao@gmail.com>
2025-07-22 16:07:46 -0300

    chore(init): intial project strocture

---
+++ database/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const conn = new Sequelize({
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+    port: process.env.DATABASE_PORT
+})
+
+module.exports = conn;
---
+++ index.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+const conn = require('./database/database.js');
+const bodyParser = require('body-parser');
+
+//contorller imports
+const categoriesController = require('./categories/CategoriesController.js');
+const articlesController = require('./articles/ArticlesController.js');
+
+//Model imports
+const Category = require('./categories/Category.js');
+const Article = require('./articles/Article.js');
+
+conn.authenticate().then(() => {
+    console.log('Database conn OK');
+}).catch(error => {
+    console.error('Database conn error:', error);
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+app.get('/', (req, res) => {
+    res.render('index' ,
+        {
+            page_title : 'Home',
+            msgError: req.query.msgError
+        });
+});
+
+
+app.use('/', categoriesController);
+
+app.use('/', articlesController);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
