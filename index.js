b88537c5cb97b1c297d176b6e6129a6cd584f444
Sol Requiao Falcao <solrequiao@gmail.com>
2025-07-31 15:33:04 -0300

    feat(route): implement GET / to fetch and articles

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const articlesController = require('./articles/ArticlesController.js');
 //Model imports
 const Category = require('./categories/Category.js');
 const Article = require('./articles/Article.js');
+const { where } = require('sequelize');
 
 conn.authenticate().then(() => {
     console.log('Database conn OK');
@@ -32,13 +33,6 @@ app.use(bodyParser.json());
 
 
 
-app.get('/', (req, res) => {
-    res.render('index' ,
-        {
-            page_title : 'Home',
-        });
-});
-
 app.get('/error', (req, res) => {
     res.render('error',
         {
@@ -46,6 +40,32 @@ app.get('/error', (req, res) => {
             msgError: req.query.msgError
         });
 });
+
+app.get('/', (req, res) => {
+
+    Article.findAll({
+        include: [{
+            model: Category, as: 'category',
+            required: true,
+            attributes: ['id', 'title']
+        }],
+        order: [['id', 'DESC']],
+        limit: 4,
+        where: {
+            published: true
+        }
+    }).then(articles => {
+        res.render('index', {
+            page_title: 'Home',
+            articles: articles,
+        });
+    }).catch(error => {
+        console.error('Error fetching articles:', error);
+        res.redirect('/error?msgError=Failed to load articles.');
+    });
+});
+
+
 
 app.use('/', categoriesController);
 
