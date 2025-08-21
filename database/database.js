fa39387383f327a0e8128c465585c85b7a9a91f3
Sol Requiao Falcao <solrequiao@gmail.com>
2025-08-21 12:55:00 -0300

    chore(db): default DATABASE_HOST to localhost if missing

--- database/database.js
+++ database/database.js
@@ -5,7 +5,7 @@ const conn = new Sequelize({
     database: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    host: process.env.DATABASE_HOST,
+    host: process.env.DATABASE_HOST || 'localhost',
     dialect: process.env.DATABASE_DIALECT,
     port: process.env.DATABASE_PORT
 })
